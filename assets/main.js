16493d5e54d6c95d57823314e4cdaffa14102ff8
Alek90 <4lek.90@gmail.com>
2021-10-20 15:16:14 +0200

    Creazione repo.
    Creazione repo e file dei linguaggi. Commentato procedimento in js.

---
+++ assets/main.js
@@ -0,0 +1,20 @@
+/*  OBBIETTIVI:
+    Creare 3 differenti griglie, rispettivamente da 100, da 81 e da 49 celle, sulla base della difficoltà scelta dall'utente.
+    Stampata la griglia corrispondente alla difficoltà, quando l'utente clicca su di una cella, questa deve assumere colore azzurro.
+*/
+
+// Mi serve una selezione della difficoltà, con valori preimpostati.
+
+// Sulla base del valore scelto, deve essere stampata una specifica griglia (magari aggiungendo una classe con display: block?)
+
+// Evidentemente tra i primi 2 passaggi andrà inserito un avvio per l'evento, magari tramite un bottone "play".
+
+/* Riepilogo: 
+    -selezione difficoltà;
+    -avvio evento tramite un bottone;
+    -stampo a schermo la griglia corrispondente alla difficoltà selezionata.
+*/
+
+/* La griglia ha le celle numerate, e deve possedere un evento interno:
+    Quando l'utente "clicca" su una cella, avvia l'evento che permette alla cella di assumere un colore predefinito (azzurro).
+*/
